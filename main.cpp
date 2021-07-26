4703cea017ebe8992c86215f37989c1c147c19b4
Kusuri Dheeraj Kumar <kusuri.dheeraj2014@gmail.com>
2021-07-26 22:37:35 +0530

    Initial Commit

---
+++ main.cpp
@@ -0,0 +1,37 @@
+/* Author : kusuriallmight */
+#include<bits/stdc++.h>
+using namespace std;
+#define IOS      ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define UMAX      100000+9
+#define F         first
+#define S         second
+#define debug(v)  for(auto &x:v)clog<<x<<" \n"[x==v.back()];
+#define print(v)  for(auto &x:v)cout<<x<<" \n"[x==v.back()];
+#define read(v)   for(auto &x:v)cin>>x;
+#define deb(x)    clog<<#x<<" is "<<x<<'\n';
+#define all(v)    v.begin(),v.end()
+#define pb        push_back
+#define pf		  push_front
+#define pii		  pair<int,int>
+using ll = long long;
+const int mod = 100000000+7;
+#define int long long int
+
+void Solve() {
+  int c;cin>>c;
+  cout<<c<<endl;
+}
+signed main() {
+	IOS;
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);  
+    freopen("output.txt", "w", stdout);
+	#endif
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		Solve();
+	} 
+	return 0;
+}
